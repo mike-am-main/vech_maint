177e8d88dbcc131f831cf85878b5810dc8820cd0
mike <mike.am.main#gmail.com>
2012-07-12 12:17:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VechMaint::Application.config.secret_token = '081c108bed07be5ceeca2ec677400ea868576b345ac1b989ec673758c983f221608eb06f7442747569d6af4d1e1fb36ca066fa7c2e0eef66f89695e1edb903c9'
