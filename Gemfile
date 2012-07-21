02343c440666f4295f001e2bcece14e5f036e21d
mike <mike.am.main#gmail.com>
2012-07-21 14:33:59 -0400

    Basic User model (including secure password)

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.6'
 gem 'bootstrap-sass', '2.0.0'
+gem 'bcrypt-ruby', '3.0.1'
 gem 'rake', '0.9.2.2'
 gem 'jquery-rails', '2.0.0'
 gem 'execjs'
@@ -15,6 +16,8 @@ group :development do
   gem 'rspec-rails', '2.10.0'
   gem 'guard-rspec', '0.5.5'
 end
+
+gem 'annotate', '~> 2.4.1.beta', group: :development
 
 group :test do
   gem 'capybara', '1.1.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 VechMaint::Application.routes.draw do
+  get "users/new"
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
 
   root :to => 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
+
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  match ':controller(/:action(/:id))(.:format)'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -57,7 +65,5 @@ VechMaint::Application.routes.draw do
 
   # See how all your routes lay out with "rake routes"
 
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  match ':controller(/:action(/:id))(.:format)'
+
 end
