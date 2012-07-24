7e6b6f56ca25815b242607f6a3f749338abe5192
mike <mike.am.main#gmail.com>
2012-07-24 15:35:07 -0400

    Finish sigh in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 VechMaint::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
@@ -8,11 +9,13 @@ VechMaint::Application.routes.draw do
 
   root :to => 'static_pages#home'
 
-  match '/signup',  to: 'users#new'
-
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   match ':controller(/:action(/:id))(.:format)'
