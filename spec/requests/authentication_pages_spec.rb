f0790e9a7f07e81bc8db644310a545394e4a2a85
mike <mike.am.main#gmail.com>
2012-07-29 06:35:10 -0400

    Corrected Errors in rspec

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -71,7 +71,7 @@ describe "Authentication" do
 
         describe "visiting the user index" do
           before { visit users_path }
-          it { should have_selector('title', text: 'All users') }
+          it { should have_selector('title', text: 'Sign in') }
         end
       end
 
